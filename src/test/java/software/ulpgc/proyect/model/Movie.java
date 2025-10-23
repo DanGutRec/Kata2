f85dd8275594c9826c7c98114999c021e412396e
Dani <164520978+DanGutRec@users.noreply.github.com>
2025-10-23 15:47:01 +0100

    Proyect created with movie model added

---
+++ src/test/java/software/ulpgc/proyect/model/Movie.java
@@ -0,0 +1,12 @@
+package software.ulpgc.proyect.model;
+
+public record Movie(String tconst,
+                    String titleType,
+                    String primaryTitle,
+                    String  originalTitle,
+                    boolean isAdult ,
+                    int startYear,
+                    int endYear,
+                    int runtimeMinutes,
+                    String[] genres) {
+}
